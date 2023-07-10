024f3077ccefd5e7d415f3f165606056e22338a7
Amartya Kumar <amartya324@gmail.com>
2023-07-10 12:21:17 +0530

    fetch and display data from tvmaze api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+
+    //https://imdb8.p.rapidapi.com/auto-complete?q=boys
+  };
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occurred: {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+
+      <div>{renderApiData()}</div>
+    </div>
+  );
 };
 
 export default Home;
