34efaef38be0de687c4abae3e17b434257dde5d1
Amartya Kumar <amartya324@gmail.com>
2023-07-17 14:20:29 +0530

    installed styled components. Create global theme with styled components. Create custom radio. used NavLink from react-router to style active link

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import Starred from './pages/Starred.';
 import Show from './pages/Show';
 import MainLayout from './components/MainLayout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalTheme } from './theme';
 
 const queryClient = new QueryClient();
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element=<MainLayout />>
-            <Route path="/" element={<Home />} />
-            <Route path="/starred" element={<Starred />} />
-          </Route>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element=<MainLayout />>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
 
-          <Route path="/show/:showId" element={<Show />} />
+            <Route path="/show/:showId" element={<Show />} />
 
-          <Route path="*" element={<div>Not found</div>} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<div>Not found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
